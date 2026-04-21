8c5467ee1f4194d90e3d89f54b27315a3542dfda
AcuGero <86161201+AcuGero@users.noreply.github.com>
2026-04-21 21:22:09 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# Репозиторий - это папка с проектом за которой следит Git
+# Commit - это сохранение состояния проекта
+# написали код - коммит
+# что то добавили - комми
+# что то сломали - назад к последнему коммиту
+# git status - следим за изменениями
+# add - выбрать
+# commit - sohranit'
+# git push/pull - запушить подтянуть изменения
+# gut log - история комимитов
+
+
+print('bebebe') 
+
+
+# asfasf
+# asfqwfsa qw cds erfv er 
+# saxc qwd 
+
+# .gitignore
+
+# .log игнорируем логи
+# .env - игнорируем env 
+# venv/ - игнорим папку venv 
+# __pycache__/
+
+
+# git remote add origin https...
+# git push -u origin main
+# git brunch -M main
+# git clone https...
